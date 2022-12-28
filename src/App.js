773ed4125600bb37894db787be8613068fe84f1b
hleonn <edhleon2001@hotmail.com>
2022-12-27 20:03:07 -0600

    Basics

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Componente from "./components/Componente.jsx";
+import Propiedades from "./components/Propiedades.jsx";
+import Estado from "./components/Estado.jsx";
+import RenderizadoCondicional from "./components/RenderizadoCondicional.jsx";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <section>
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Editar <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </section>
+      <section>
+        <Componente msg="Hello, Hello, I am a express function Component from a prop"/>
+      <hr></hr>
+        <Propiedades 
+        cadena = "This a string"
+        numero = {19}
+        booleano = {true}
+        arreglo ={[2,4,6]}
+        funcion = {(num)=>num*num}
+        objeto = {{nombre:"Tato", correo:"tatoleon2020@gmail.com"}}
+        elementoReact = {<i>Esto es un elemento React</i>}
+        componenteReact = {<Componente msg="Soy un componente pasado como Props"/>}
+        />
+        <hr></hr>
+        <Estado/>
+        <hr></hr>
+        <RenderizadoCondicional/> 
+        <hr></hr> 
+      </section>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
