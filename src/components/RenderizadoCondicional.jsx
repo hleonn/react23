90f77a005fe986f012679262690f8dd971691229
hleonn <edhleon2001@hotmail.com>
2022-12-27 20:44:18 -0600

    conditional render

--- src/components/RenderizadoCondicional.jsx
+++ src/components/RenderizadoCondicional.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
+function Login(){
+return(
+  <div>
+    <h3>loginn</h3>
+  </div>
+);
+}
+
+function Logout(){
+  return(
+    <div>
+    <h3>logoutt</h3>
+    </div>
+  );
+}
 export default class RenderizadoCondicional extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      session:true,
+    };
+  }
   render(){
     return(
       <div>
         <h2>Renderizado Condicional</h2>
+        {this.state.session?<Login/>:<Logout/>}
+        
+        
       </div>
     )
   }
