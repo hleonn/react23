c35342c757cb1b53345a44e8be53cb5857d447a2
hleonn <edhleon2001@hotmail.com>
2022-12-30 08:38:35 -0600

    Ajax&Apis

--- src/components/AjaxApis.jsx
+++ src/components/AjaxApis.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 function Pokemon(props) {
   return (
     <figure>
-      <img src={props.avatar} alt={props.name} />;
-      <figcaption>{props.name}</figcaption>
+      <img src={props.avatar} alt={props.name} />
+       <figcaption><h5>{props.name}</h5></figcaption>
     </figure>
   );//
-};//
+}//
 
 export default class AjaxApis extends Component {
   //Property Initializers
@@ -17,7 +17,7 @@ export default class AjaxApis extends Component {
 
   componentDidMount() {
     let url = "https://pokeapi.co/api/v2/pokemon/";
-    fetch(url)
+    fetch(url)//ir por los datos de la api
       .then((res) => res.json())
       .then((json) => {
         //console.log(json);
@@ -34,21 +34,26 @@ export default class AjaxApis extends Component {
                 avatar: json.sprites.front_default,
               };
               let pokemons = [...this.state.pokemons, pokemon];
-              this.setState({ pokemons })
-            });});});}
-  function = (e) => {
-    this.setState({ pokemons: this.state.pokemons })
-  }
+              this.setState({ pokemons });
+            });
+          });
+        });
+      }
+  
   render() {
     return (
       <>
-        <h3>Peticiones asincronas en componentes de clase</h3>
-        {this.state.pokemons.length === 0 ? (<h3>Cargando</h3>):(
+        <h2>Peticiones asincronas en componentes de clase</h2>
+        {this.state.pokemons.length === 0 ? (
+          <h3>Cargando...</h3>
+          ):(
             this.state.pokemons.map((el) => (
               <Pokemon key={el.id} name={el.name} avatar={el.avatar} />
-            )))}}
-</>
+            ))
+            )}
+      </>
     );
   }//
 }
+            
 //"https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20"
